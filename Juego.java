9ad4fa575f7cbcc149a5797dc8b38b32c22ffcb9
Bamo0507 <bryanalbertomo@gmail.com>
2023-08-04 22:52:48 -0600

    Juego funcional

--- Juego.java
+++ Juego.java
@@ -1,6 +1,7 @@
 public class Juego {
-    private static boolean Fin = false;
-    private int Check = 0;
+    private static int Check = 0;
+    private static int x;
+    private static int y;
     public static void main(String[] args) {
         Dado Dado1 = new Dado();
         Dado Dado2 = new Dado();
@@ -10,31 +11,212 @@ public class Juego {
         Jugador Player4 = new Jugador();
         
 
+        //Ciclo principal del juego
+        while(true) {
 
-        while(Fin == false) {
-            System.out.println("Es el turno del Player1");
+            System.out.println("\nPlayer1:");
+            //Ciclo para el juego del Player1
             while(true) {
+                //Se verifica si el jugador desea continuar
                 Player1.Continuar();
-                if (Player1.getDecision() == "1"){
+
+                //Se lanzan los dados si se sigue
+                if (Player1.getDecision().equals("1")){
                     Dado1.Lanzamiento();
                     Dado2.Lanzamiento();
-                    Player1.Puntaje(Dado1.getCara(), Dado2.getCara());
-                    Check = 
-                    Player1.toString();
-                else if (Player1.getDecision() != "1"){
+                    x = Dado1.getCara();
+                    y = Dado2.getCara();
+
+                //Agregamos los puntos al punteo, y se elimina todo el puntaje si se obtiene 1 en algún dado
+                    Player1.Puntaje(x, y);
+                    System.out.println(Player1.toString());
+                    System.out.println("-------------------------------");
+
+                //Se verifica que aún no se haya ganado
+                    if (Player1.getPuntos() >= 100){
+                        System.out.println("¡Felicidades por tu victoria en el juego de PIG!");
+                        System.out.println("Eres el maestro de los dados. Disfruta de tu triunfo!\n");
+                        System.exit(0);
+                        break;
+                    }
+
+                    Check = Check + (x + y);
+                
+                    //Se verifica que no se haya sacado 1 al ver el punteo tras lanzar los dados, sino se salta turno
+                    if (Player1.getPuntos() == 0) {
+                        Check = 0;
+                        break;
+                    }
+
+                    //Se revisa que la suma del lanzamiento no sea mayor a 20
+                    if (Check >= 20){
+                        Check = 0;
+                        System.out.println("Has conseguido 20 o más puntos, es el turno del siguiente jugador.");
+                        System.out.println("-------------------------------");
+                        break;
+                    }
+
+                    }
+
+                //Código a mostrar en caso de qué no se quiera seguir tirando
+                else if (!Player1.getDecision().equals("1")){
+                    Check = 0;
                     System.out.println("Pasando al siguiente jugador.\n");
                     break;
-                }
-
-            
-            
-
-            
-
+                    } 
             }
 
+            
+            System.out.println("\nPlayer2:");
+            //Ciclo para el juego del Player2
+            while(true) {
+                //Se verifica si el jugador desea continuar
+                Player2.Continuar();
+
+                //Se lanzan los dados si se sigue
+                if (Player2.getDecision().equals("1")){
+                    Dado1.Lanzamiento();
+                    Dado2.Lanzamiento();
+                    x = Dado1.getCara();
+                    y = Dado2.getCara();
+
+                //Agregamos los puntos al punteo, y se elimina todo el puntaje si se obtiene 1 en algún dado
+                    Player2.Puntaje(x, y);
+                    System.out.println(Player2.toString());
+                    System.out.println("-------------------------------");
+
+                //Se verifica que aún no se haya ganado
+                    if (Player2.getPuntos() >= 100){
+                        System.out.println("¡Felicidades por tu victoria en el juego de PIG!");
+                        System.out.println("Eres el maestro de los dados. Disfruta de tu triunfo!\n");
+                        System.exit(0);
+                        break;
+                    }
+
+                    Check = Check + (x + y);
+                
+                    //Se verifica que no se haya sacado 1 al ver el punteo tras lanzar los dados, sino se salta turno
+                    if (Player2.getPuntos() == 0) {
+                        Check = 0;
+                        break;
+                    }
+
+                    //Se revisa que la suma del lanzamiento no sea mayor a 20
+                    if (Check >= 20){
+                        Check = 0;
+                        System.out.println("Has conseguido 20 o más puntos, es el turno del siguiente jugador.");
+                        System.out.println("-------------------------------");
+                        break;
+                    }
+                }
+                //Código a mostrar en caso de qué no se quiera seguir tirando
+                else if (!Player2.getDecision().equals("1")){
+                    Check = 0;
+                    System.out.println("Pasando al siguiente jugador.\n");
+                    break;
+                } 
+            }
+
+            System.out.println("\nPlayer3:");
+            //Ciclo para el juego del Player1
+            while(true) {
+                //Se verifica si el jugador desea continuar
+                Player3.Continuar();
+
+                //Se lanzan los dados si se sigue
+                if (Player3.getDecision().equals("1")){
+                    Dado1.Lanzamiento();
+                    Dado2.Lanzamiento();
+                    x = Dado1.getCara();
+                    y = Dado2.getCara();
+
+                //Agregamos los puntos al punteo, y se elimina todo el puntaje si se obtiene 1 en algún dado
+                    Player3.Puntaje(x, y);
+                    System.out.println(Player3.toString());
+                    System.out.println("-------------------------------");
+
+                //Se verifica que aún no se haya ganado
+                    if (Player3.getPuntos() >= 100){
+                        System.out.println("¡Felicidades por tu victoria en el juego de PIG!");
+                        System.out.println("Eres el maestro de los dados. Disfruta de tu triunfo!\n");
+                        System.exit(0);
+                        break;
+                    }
+
+                    Check = Check + (x + y);
+                
+                    //Se verifica que no se haya sacado 1 al ver el punteo tras lanzar los dados, sino se salta turno
+                    if (Player3.getPuntos() == 0) {
+                        Check = 0;
+                        break;
+                    }
+
+                    //Se revisa que la suma del lanzamiento no sea mayor a 20
+                    if (Check >= 20){
+                        Check = 0;
+                        System.out.println("Has conseguido 20 o más puntos, es el turno del siguiente jugador.");
+                        System.out.println("-------------------------------");
+                        break;
+                    }
+                }
+                //Código a mostrar en caso de qué no se quiera seguir tirando
+                else if (!Player3.getDecision().equals("1")){
+                    Check = 0;
+                    System.out.println("Pasando al siguiente jugador.\n");
+                    break;
+                } 
+            }
+
+            System.out.println("\nPlayer4:");
+            //Ciclo para el juego del Player1
+            while(true) {
+                //Se verifica si el jugador desea continuar
+                Player4.Continuar();
+
+                //Se lanzan los dados si se sigue
+                if (Player4.getDecision().equals("1")){
+                    Dado1.Lanzamiento();
+                    Dado2.Lanzamiento();
+                    x = Dado1.getCara();
+                    y = Dado2.getCara();
+
+                //Agregamos los puntos al punteo, y se elimina todo el puntaje si se obtiene 1 en algún dado
+                    Player4.Puntaje(x, y);
+                    System.out.println(Player4.toString());
+                    System.out.println("-------------------------------");
+
+                //Se verifica que aún no se haya ganado
+                    if (Player4.getPuntos() >= 100){
+                        System.out.println("¡Felicidades por tu victoria en el juego de PIG!");
+                        System.out.println("Eres el maestro de los dados. Disfruta de tu triunfo!\n");
+                        System.exit(0);
+                        break;
+                    }
+
+                    Check = Check + (x + y);
+                
+                    //Se verifica que no se haya sacado 1 al ver el punteo tras lanzar los dados, sino se salta turno
+                    if (Player4.getPuntos() == 0) {
+                        Check = 0;
+                        break;
+                    }
+
+                    //Se revisa que la suma del lanzamiento no sea mayor a 20
+                    if (Check >= 20){
+                        Check = 0;
+                        System.out.println("Has conseguido 20 o más puntos, es el turno del siguiente jugador.");
+                        System.out.println("-------------------------------");
+                        break;
+                    }
+                }
+                //Código a mostrar en caso de qué no se quiera seguir tirando
+                else if (!Player4.getDecision().equals("1")){
+                    Check = 0;
+                    System.out.println("Pasando al siguiente jugador.\n");
+                    break;
+                } 
+            }
     }
 }
 
-}
 }
--- Jugador.java
+++ Jugador.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 public class Jugador {
+    //Atributos
     private int Puntos;
     private String Decision;
     private int Suma = 0;
-    
     Scanner teclado = new Scanner(System.in);
 
     //Constructor
@@ -13,49 +13,51 @@ public class Jugador {
     }
 
     //Setters y Getters
-
     public void setPuntos(int Puntos){
         this.Puntos = Puntos;
     }
-
     public int getPuntos() {
         return this.Puntos;
     }
-
     public void setDecision(String Decision){
         this.Decision = Decision;
     }
-
     public String getDecision(){
         return this.Decision;
     }
-
+    public void setSuma(int Suma){
+        this.Suma = Suma;
+    }
     public int getSuma() {
         return this.Suma;
     }
 
     //Demás métodos
-
     public void Continuar() {
-        System.out.println("¿Deseas seguir lanzando? Colocar 1 si desea seguir o cualquier otra tecla si quiere parar.\n");
-        Decision = teclado.nextLine();
+        System.out.println("\n¿Deseas seguir lanzando? Colocar 1 si desea seguir o cualquier otra tecla si quiere parar.");
+        this.Decision = teclado.nextLine();
         
     }
-
     public void Puntaje(int Dado1, int Dado2){
         Suma = Dado1 + Dado2;
+        System.out.println("-------------------------------");
+        System.out.println("Has obtenido un " + Dado1 + " y un " + Dado2 + ".");
+        System.out.println("-------------------------------");
+
         if (Dado1 == 1 || Dado2 == 1){
             setPuntos(0);
-            System.out.println("Lamentablemente has obtenido un 1, tus puntos están en 0 :( \n");
+            System.out.println("Oh no, qué mala suerte!!! Es un 1 :(");
+            System.out.println("Es el turno del siguiente jugador.");
         }
         else {
             Puntos += Suma;
-            Suma = 0;
+            setSuma(0);
+
         }
     }
 
     public String toString() {
-        return "Tu punteo actual es de: " + this.Puntos + ".\n";
+        return "Tu punteo actual es de: " + this.Puntos + ".";
     }
 
 }
